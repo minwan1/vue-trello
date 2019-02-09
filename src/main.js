4c2d3799ae2589c1009bc58fb04fff9d5dd7e59b
minwan <ansatgol@gmail.com>
2019-02-09 10:30:22 +0900

    second commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+const Login = {template: '<div>Login Page</div>'};
+
+const routes = {
+  '/' : App,
+  '/login' : Login
+};
+
+new Vue({
+  el: '#app',
+  computed:{
+    VueComponent(){
+      return routes[window.location.pathname] || { template: '<div> Page not found</div>'}
+    }
+  },
+  render(h){
+    return h(this.VueComponent)
+  }
+});

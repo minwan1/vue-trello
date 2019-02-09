66f30af8bc4c95d48061a5d8cc7d370dea6b3c56
minwan <ansatgol@gmail.com>
2019-02-09 11:24:20 +0900

    add routing-2

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = {template: '<div>Login Page</div>'};
+const Notfound = {template: '<div> Page not found</div>'};
 
-const routes = {
-  '/' : App,
-  '/login' : Login
-};
+
+const routes = [
+  { path: '/', component: App },
+  { path: '/login', component: Login },
+  {path: '*', component: Notfound} //우선순위가 제일 낮음
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // `routes: routes`의 줄임
+})
+
 
 new Vue({
   el: '#app',
-  computed:{
-    VueComponent(){
-      return routes[window.location.pathname] || { template: '<div> Page not found</div>'}
-    }
-  },
-  render(h){
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 });

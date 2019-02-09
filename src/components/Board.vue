734dc055c6d4be43691389bb23883f4b6ff20105
wan.jin <wan.jin@kakaoorp.com>
2019-02-09 21:19:14 +0900

    ajax 로딩추가

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
     Board
-    <div>bid : {{bid}}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card 1</router-link>
-    <hr />
-    <router-view></router-view>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid : {{bid}}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+      <hr />
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "Board",
-  data() {
-    return {
-      bid: 0
-    }
-  },
+  data: () => ({
+      bid: 0,
+      loading: false
+  }),
   computed : {
     path1(){
       return `/b/${this.bid}/c/1`;
     }
-
   },
   created(){
-    this.bid = this.$route.params.bid;
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      }, 500)
+    }
   }
 }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     Card
-    <div>cid: {{cid}}</div>
+    <div v-if="loading">loading card ...</div>
+    <div v-else>cid : {{cid}}</div>
   </div>
 </template>
 
@@ -10,13 +11,27 @@ export default {
    name: "Card",
   data() {
      return {
-       cid: 0
+       cid: 0,
+       loading: false
      }
   },
   watch: {
-     '$route'() {
-       this.cid = this.$route.params.cid;
+     '$route': {
+       handler: 'fetchData',
+       immediate: true
      }
+  },
+  methods: {
+    fetchData(){
+      this.loading = true;
+      setTimeout(()=> {
+        this.cid = this.$route.params.cid;
+        this.loading = false;
+
+      }, 500)
+
+    }
+
   }
 }
 </script>

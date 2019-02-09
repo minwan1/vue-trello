5fc9f5ca2f09ea7a8b7b31a550b32e44b836522a
minwan <ansatgol@gmail.com>
2019-02-09 11:37:02 +0900

    add router-link function

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    여기서부터 코드가 시작됩니다.
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
-  name: 'app'
+  name: 'app',
+  components: {Navbar}
 }
 </script>
